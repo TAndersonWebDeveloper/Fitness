6a1ab59fbbb37077b75f83d7bbd20b123bacefc7
Tyler Anderson <anderson.tyler10@gmail.com>
2022-07-28 20:02:22 -0500

    open and close modal functionality

--- app.js
+++ app.js
@@ -32,3 +32,30 @@ aboutItem.forEach((item) => {
 });
 
 observer.observe(aboutHeader);
+
+//Modal functionality
+const signUpBtn = document.querySelectorAll(".signup-btn");
+const modal = document.querySelector(".modal");
+const closeModalBtn = document.querySelector(".close-modal");
+const modalSignupBtn = document.querySelector(".modal-signup-btn");
+let nameInput = document.querySelector("#name");
+
+nameInput.addEventListener("change", (e) => {
+  nameInput = e.target.value;
+});
+
+for (i = 0; i < signUpBtn.length; i++) {
+  signUpBtn[i].addEventListener("click", () => {
+    modal.classList.remove("hide");
+  });
+}
+closeModalBtn.addEventListener("click", () => {
+  modal.classList.add("hide");
+});
+modalSignupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (nameInput.length > 0) {
+    modal.classList.add("hide");
+    nameInput.value = "";
+  }
+});
