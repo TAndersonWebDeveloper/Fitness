d3ba0f7b1bd80230e63ed00891d91bc2132fbca2
Tyler Anderson <anderson.tyler10@gmail.com>
2022-07-28 21:41:18 -0500

    add weather api

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-//Slick Slider for classes section
+let clientName;
+const welcomeBackMessage = document.querySelector(".welcome-back");
+if (localStorage.getItem("fName")) {
+  clientName = localStorage.getItem("fName");
+  welcomeBackMessage.innerHTML = `Welcome Back ${clientName}!`;
+}
+// Slick Slider for classes section
 $(function () {
   $(".single-item").slick({
     infinite: true,
@@ -14,11 +20,9 @@ $(function () {
 //Intersection observers
 const aboutHeader = document.querySelector(".about h2");
 const aboutItem = document.querySelectorAll(".about-item");
-
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      console.log(entry);
       entry.target.classList.toggle("show", entry.isIntersecting);
     });
   },
@@ -38,6 +42,10 @@ const signUpBtn = document.querySelectorAll(".signup-btn");
 const modal = document.querySelector(".modal");
 const closeModalBtn = document.querySelector(".close-modal");
 const modalSignupBtn = document.querySelector(".modal-signup-btn");
+const modalHeader = document.querySelector(".modal h2");
+const signupForm = document.querySelector(".modal form");
+const confirmationMessage = document.querySelector(".confirmation-msg");
+const thumbsUp = document.querySelector(".thumb");
 let nameInput = document.querySelector("#name");
 
 nameInput.addEventListener("change", (e) => {
@@ -54,8 +62,41 @@ closeModalBtn.addEventListener("click", () => {
 });
 modalSignupBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  if (nameInput.length > 0) {
-    modal.classList.add("hide");
-    nameInput.value = "";
+  if (nameInput.length > 0 && nameInput.length < 15) {
+    signupForm.classList.add("hide");
+    modalHeader.classList.add("hide");
+    confirmationMessage.classList.remove("hide");
+    thumbsUp.classList.add("approve");
+    clientName = nameInput;
+    welcomeBackMessage.innerHTML = `Thanks for signing up ${clientName}!`;
+    localStorage.setItem("fName", nameInput);
+    setTimeout(() => {
+      modal.classList.add("hide");
+    }, 5000);
+  }
+});
+const checkWeatherBtn = document.querySelector(".check-weather-btn");
+const highTempOutput = document.querySelector(".temp-high");
+let apiKey = "c50fe6a08da5a2fb3b65643955014b1d";
+let imgUrlStart = "http://openweathermap.org/img/wn/";
+let lat;
+let long;
+let endpoint;
+async function getWeather(lat, long) {
+  let object = await fetch(endpoint);
+  let response = await object.json().then((response) => {
+    highTempOutput.innerHTML = `High: ${response.main.temp_max}`;
+  });
+}
+
+checkWeatherBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  navigator.geolocation.getCurrentPosition(success);
+  async function success(location) {
+    lat = await location.coords.latitude;
+    long = await location.coords.longitude;
+    endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${apiKey}`;
+    console.log(lat);
+    getWeather();
   }
 });
